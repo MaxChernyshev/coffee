ef0a51f963db2aab6806276befe391b4b5594074
MaxChernyshev <maksim.chernyshev@gmail.com>
2023-09-03 00:25:48 +0200

    MachineTypes Model Resource added

---
+++ app/Filament/Resources/MachineTypeResource/Pages/EditMachineType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\MachineTypeResource\Pages;
+
+use App\Filament\Resources\MachineTypeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditMachineType extends EditRecord
+{
+    protected static string $resource = MachineTypeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
